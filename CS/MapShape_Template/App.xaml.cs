8b2b7135d68df6ac0291cdfe61fa4ea6400a975e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MapShape_Template/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace MapShape_Template {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to provide a custom pushpin for a map
+
+
+<p>This example demonstrates  how to implement a custom pushpin using its template. <br />
+</p>
+
+<br/>
+
+
